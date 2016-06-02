4047991a428f9b6c92ac703511353e3b8994e8b1
cor <cor@pruijs.nl>
2016-06-02 22:59:15 +0200

    Convert to PostgreSQL syntax

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
 FROM postgres:9.5.3
+
+COPY schema.sql /tmp/
+
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-	id integer primary key autoincrement,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id serial primary key,
 	username text not null,
 	email text not null,
 	email_verified boolean not null,
@@ -8,43 +8,43 @@ CREATE TABLE user (
 );
 
 CREATE UNIQUE INDEX user_username_unique
-on user (username);
+on users (username);
 
-DROP TABLE IF EXISTS list;
-CREATE TABLE list (
-	id integer primary key autoincrement,
+DROP TABLE IF EXISTS lists;
+CREATE TABLE lists (
+	id serial primary key,
 	user_id integer not null,
 	listname text not null,
 	language_1_tag text not null,
 	language_2_tag text not null,
 	shared integer not null,
-    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX list_name_unique_for_user
-on list (user_id, listname);
+on lists (user_id, listname);
 
-DROP TABLE IF EXISTS translation;
-CREATE TABLE translation (
-	id integer primary key autoincrement not null,
+DROP TABLE IF EXISTS translations;
+CREATE TABLE translations (
+	id serial primary key,
 	list_id integer not null,
 	language_1_text text not null,
 	language_2_text text not null,
-    FOREIGN KEY(list_id) REFERENCES list(id) ON DELETE CASCADE
+    FOREIGN KEY(list_id) REFERENCES lists(id) ON DELETE CASCADE
 );
 
 -- TODO: make user_1_id and user_2_id a unique combo
-DROP TABLE IF EXISTS friendship;
-CREATE TABLE friendship (
+DROP TABLE IF EXISTS friendships;
+CREATE TABLE friendships (
 	user_1_id integer not null,
 	user_2_id integer not null,
-	FOREIGN KEY(user_1_id) REFERENCES user(id) ON DELETE CASCADE,
-	FOREIGN KEY(user_2_id) REFERENCES user(id) ON DELETE CASCADE
+	FOREIGN KEY(user_1_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY(user_2_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS auth_tokens;
 CREATE TABLE auth_tokens (
-    id integer primary key autoincrement not null,
+    id serial primary key,
     selector text not null,
     token text not null,
     expires text not null
